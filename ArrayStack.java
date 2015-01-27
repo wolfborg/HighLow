bf649a14c7519b1b4af8daee67d536c4156da95c
wolfborg <kingderekx@yahoo.com>
2015-01-27 15:50:59 -0500

    ArrayStack functions & variables

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,42 +1,79 @@
 package Maze;
 
+import java.util.Arrays;
+
 public class ArrayStack<E> implements StackInterface<E>
-{	
-	public ArrayStack()
+{
+	private E[] stack;
+	private int topIndex;
+	private static final int defaultInitialCapacity = 25;
+	
+	private void ensureCapacity()
 	{
-		
+		if(topIndex==stack.length-1){
+			stack = Arrays.copyOf(stack, 2*stack.length);
+		}
 	}
 	
+	public ArrayStack()
+	{
+		this(defaultInitialCapacity);
+	}
+	
+	public ArrayStack(int initialCapacity)
+	{
+		@SuppressWarnings("unchecked")
+		E[] tempStack = (E[])new Object[initialCapacity];
+		stack = tempStack;
+		topIndex = -1;
+	}
 
 	public int size()
 	{
+		int size = 0;
 		
-		return 0;
+		for(int i=0;i<stack.length;i++){
+			if(!stack[i].equals(null)){
+				size++;
+			}
+		}
+		
+		return size;
 	}
-
 	
 	public boolean isEmpty()
 	{
-		
-		return false;
+		return (size()==0);
 	}
-
 	
 	public E peek()
 	{
+		E top = null;
 		
-		return null;
+		if(!isEmpty()){
+			top = stack[topIndex];
+		}
+		
+		return top;
 	}
-
 	
-	public void push(E element)
+	public void push(E newEntry)
 	{
-		
+		ensureCapacity();
+		topIndex++;
+		stack[topIndex] = newEntry;
 	}
 
 	public E pop()
 	{
+		E top = null;
 		
-		return null;
+		if(!isEmpty()){
+			top = stack[topIndex];
+			stack[topIndex] = null;
+			topIndex--;
+		}
+		
+		return top;
 	}
 }
