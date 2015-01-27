25392bd8036081afb1883bac878a0c124815f1ed
wolfborg <kingderekx@yahoo.com>
2015-01-26 22:50:44 -0500

    ArrayStack class

---
+++ ArrayStack.java
@@ -0,0 +1,42 @@
+package Maze;
+
+public class ArrayStack<E> implements StackInterface<E>
+{	
+	public ArrayStack()
+	{
+		
+	}
+	
+
+	public int size()
+	{
+		
+		return 0;
+	}
+
+	
+	public boolean isEmpty()
+	{
+		
+		return false;
+	}
+
+	
+	public E peek()
+	{
+		
+		return null;
+	}
+
+	
+	public void push(E element)
+	{
+		
+	}
+
+	public E pop()
+	{
+		
+		return null;
+	}
+}
