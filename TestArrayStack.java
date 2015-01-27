53eb79800716e75569b8364a21d1e946f69fe884
wolfborg <kingderekx@yahoo.com>
2015-01-27 16:31:46 -0500

    TestArrayStack class

---
+++ TestArrayStack.java
@@ -0,0 +1,36 @@
+package Maze;
+
+public class TestArrayStack
+{
+	public static void main(String[] args)
+	{
+		StackInterface<String> stack = new ArrayStack<String>();
+		
+		
+	}
+	
+	private static void testSize(StackInterface<String> stack, int correct)
+	{
+		
+	}
+	
+	private static void testIsEmpty(StackInterface<String> stack, boolean correct)
+	{
+		
+	}
+	
+	private static void testPeek(StackInterface<String> stack, String correct)
+	{
+		
+	}
+	
+	private static void testPush(StackInterface<String> stack, String correct)
+	{
+		
+	}
+	
+	private static void testPop(StackInterface<String> stack, String correct)
+	{
+		
+	}
+}
